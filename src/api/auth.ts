f13d50d51310da3217c910125237f7143234826a
Quiller <id.ivan.romancik@gmail.com>
2026-05-24 18:28:35 +0200

    fix the broken code

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,16 +3,14 @@ import { IncomingHttpHeaders } from "http";
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
   if (!authHeader) {
-    // return null;
-    return "huh";
+    return null;
   }
 
   const splitAuth = authHeader.split(" ");
   if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
-    // return null;
-    return "huh";
+    return null;
+
   }
 
-  // return splitAuth[1];
-  return "huh";
+  return splitAuth[1];
 }
